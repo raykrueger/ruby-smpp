7f6c01efd857a202de76c803220aaa0521b67ea3
raykrueger <raykrueger@gmail.com>
2011-02-27 13:10:42 -0600

    Refactor Smpp::Base, Transceiver and Receiver
    There was an insane amount of duplication between the Receiver and Transceiver implementations. I've moved all of the
    PDU processing logic into Base where it belongs. Regardless of how we are bound, PDU processing is actually the same.

--- lib/smpp/base.rb
+++ lib/smpp/base.rb
@@ -13,6 +13,22 @@ module Smpp
     # :bound or :unbound
     attr_accessor :state
     
+    def initialize(config, delegate)
+      @state = :unbound
+      @config = config
+      @data = ""
+      @delegate = delegate
+      @pdr_storage = pdr_storage={}
+
+      # Array of un-acked MT message IDs indexed by sequence number.
+      # As soon as we receive SubmitSmResponse we will use this to find the 
+      # associated message ID, and then create a pending delivery report.
+      @ack_ids = {}
+
+      ed = @config[:enquire_link_delay_secs] || 5
+      comm_inactivity_timeout = 2 * ed
+    end
+
     # queries the state of the transmitter - is it bound?
     def unbound?
       @state == :unbound
@@ -34,11 +50,6 @@ module Smpp
       @@logger
     end
     
-    def initialize(config)
-      @state = :unbound
-      @config = config
-      @data = ""
-    end
     
     # invoked by EventMachine when connected
     def post_init
@@ -140,6 +151,94 @@ module Smpp
         logger.warn "Received NACK! (error code #{pdu.error_code})."
         # we don't take this lightly: close the connection
         close_connection
+      when Pdu::DeliverSm
+        write_pdu(Pdu::DeliverSmResponse.new(pdu.sequence_number))
+        logger.debug "ESM CLASS #{pdu.esm_class}"
+        if pdu.esm_class != 4
+          # MO message
+          if @delegate.respond_to?(:mo_received)
+            @delegate.mo_received(self, pdu)
+          end
+        else
+          # Delivery report
+          if @delegate.respond_to?(:delivery_report_received)
+            @delegate.delivery_report_received(self, pdu)
+          end
+        end     
+      when Pdu::BindTransceiverResponse
+        case pdu.command_status
+        when Pdu::Base::ESME_ROK
+          logger.debug "Bound OK."
+          @state = :bound
+          if @delegate.respond_to?(:bound)
+            @delegate.bound(self)
+          end
+        when Pdu::Base::ESME_RINVPASWD
+          logger.warn "Invalid password."
+          # scheduele the connection to close, which eventually will cause the unbound() delegate 
+          # method to be invoked.
+          close_connection
+        when Pdu::Base::ESME_RINVSYSID
+          logger.warn "Invalid system id."
+          close_connection
+        else
+          logger.warn "Unexpected BindTransceiverResponse. Command status: #{pdu.command_status}"
+          close_connection
+        end
+      when Pdu::SubmitSmResponse
+        mt_message_id = @ack_ids.delete(pdu.sequence_number)
+        if !mt_message_id
+          raise "Got SubmitSmResponse for unknown sequence_number: #{pdu.sequence_number}"
+        end
+        if pdu.command_status != Pdu::Base::ESME_ROK
+          logger.error "Error status in SubmitSmResponse: #{pdu.command_status}"
+          if @delegate.respond_to?(:message_rejected)
+            @delegate.message_rejected(self, mt_message_id, pdu)
+          end
+        else
+          logger.info "Got OK SubmitSmResponse (#{pdu.message_id} -> #{mt_message_id})"
+          if @delegate.respond_to?(:message_accepted)
+            @delegate.message_accepted(self, mt_message_id, pdu)
+          end        
+        end
+        # Now we got the SMSC message id; create pending delivery report.
+        @pdr_storage[pdu.message_id] = mt_message_id            
+      when Pdu::SubmitMultiResponse
+        mt_message_id = @ack_ids[pdu.sequence_number]
+        if !mt_message_id
+          raise "Got SubmitMultiResponse for unknown sequence_number: #{pdu.sequence_number}"
+        end
+        if pdu.command_status != Pdu::Base::ESME_ROK
+          logger.error "Error status in SubmitMultiResponse: #{pdu.command_status}"
+          if @delegate.respond_to?(:message_rejected)
+            @delegate.message_rejected(self, mt_message_id, pdu)
+          end
+        else
+          logger.info "Got OK SubmitMultiResponse (#{pdu.message_id} -> #{mt_message_id})"
+          if @delegate.respond_to?(:message_accepted)
+            @delegate.message_accepted(self, mt_message_id, pdu)
+          end
+        end
+      when Pdu::BindReceiverResponse
+        case pdu.command_status
+        when Pdu::Base::ESME_ROK
+          logger.debug "Bound OK."
+          @state = :bound
+          if @delegate.respond_to?(:bound)
+            @delegate.bound(self)
+          end
+        when Pdu::Base::ESME_RINVPASWD
+          logger.warn "Invalid password."
+          # scheduele the connection to close, which eventually will cause the unbound() delegate 
+          # method to be invoked.
+          close_connection
+        when Pdu::Base::ESME_RINVSYSID
+          logger.warn "Invalid system id."
+          close_connection
+        else
+          logger.warn "Unexpected BindReceiverResponse. Command status: #{pdu.command_status}"
+          close_connection
+        end
       else
         logger.warn "(#{self.class.name}) Received unexpected PDU: #{pdu.to_human}."
         close_connection
--- lib/smpp/transceiver.rb
+++ lib/smpp/transceiver.rb
@@ -1,4 +1,3 @@
-
 # The SMPP Transceiver maintains a bidirectional connection to an SMSC.
 # Provide a config hash with connection options to get started. 
 # See the sample_gateway.rb for examples of config values.
@@ -13,23 +12,6 @@
 #   unbound(transceiver)
 
 class Smpp::Transceiver < Smpp::Base
-
-  def initialize(config, delegate, pdr_storage={})
-    super(config)
-    @delegate = delegate
-    @pdr_storage = pdr_storage
-    
-    # Array of un-acked MT message IDs indexed by sequence number.
-    # As soon as we receive SubmitSmResponse we will use this to find the 
-    # associated message ID, and then create a pending delivery report.
-    @ack_ids = {}
-    
-    ed = @config[:enquire_link_delay_secs] || 5
-    comm_inactivity_timeout = 2 * ed
-  rescue Exception => ex
-    logger.error "Exception setting up transceiver: #{ex} at #{ex.backtrace.join("\n")}"
-    raise
-  end
 
   # Send an MT SMS message. Delegate will receive message_accepted callback when SMSC 
   # acknowledges, or the message_rejected callback upon error
@@ -60,7 +42,10 @@ class Smpp::Transceiver < Smpp::Base
       0.upto(parts.size-1) do |i|
         udh = sprintf("%c", 5)            # UDH is 5 bytes.
         udh << sprintf("%c%c", 0, 3)      # This is a concatenated message 
+
+        #TODO Figure out why this needs to be an int here, it's a string elsewhere
         udh << sprintf("%c", message_id)  # The ID for the entire concatenated message
+
         udh << sprintf("%c", parts.size)  # How many parts this message consists of
         udh << sprintf("%c", i+1)         # This is part i+1
         
@@ -96,82 +81,6 @@ class Smpp::Transceiver < Smpp::Base
     else
       raise InvalidStateException, "Transceiver is unbound. Cannot send MT messages."
     end
-  end
-
-  # a PDU is received. Parse it and invoke delegate methods.
-  def process_pdu(pdu)
-    case pdu
-    when Pdu::DeliverSm
-      write_pdu(Pdu::DeliverSmResponse.new(pdu.sequence_number))
-      logger.debug "ESM CLASS #{pdu.esm_class}"
-      if pdu.esm_class != 4
-        # MO message
-        if @delegate.respond_to?(:mo_received)
-          @delegate.mo_received(self, pdu)
-        end
-      else
-        # Delivery report
-        if @delegate.respond_to?(:delivery_report_received)
-          @delegate.delivery_report_received(self, pdu)
-        end
-      end     
-    when Pdu::BindTransceiverResponse
-      case pdu.command_status
-      when Pdu::Base::ESME_ROK
-        logger.debug "Bound OK."
-        @state = :bound
-        if @delegate.respond_to?(:bound)
-          @delegate.bound(self)
-        end
-      when Pdu::Base::ESME_RINVPASWD
-        logger.warn "Invalid password."
-        # scheduele the connection to close, which eventually will cause the unbound() delegate 
-        # method to be invoked.
-        close_connection
-      when Pdu::Base::ESME_RINVSYSID
-        logger.warn "Invalid system id."
-        close_connection
-      else
-        logger.warn "Unexpected BindTransceiverResponse. Command status: #{pdu.command_status}"
-        close_connection
-      end
-    when Pdu::SubmitSmResponse
-      mt_message_id = @ack_ids.delete(pdu.sequence_number)
-      if !mt_message_id
-        raise "Got SubmitSmResponse for unknown sequence_number: #{pdu.sequence_number}"
-      end
-      if pdu.command_status != Pdu::Base::ESME_ROK
-        logger.error "Error status in SubmitSmResponse: #{pdu.command_status}"
-        if @delegate.respond_to?(:message_rejected)
-          @delegate.message_rejected(self, mt_message_id, pdu)
-        end
-      else
-        logger.info "Got OK SubmitSmResponse (#{pdu.message_id} -> #{mt_message_id})"
-        if @delegate.respond_to?(:message_accepted)
-          @delegate.message_accepted(self, mt_message_id, pdu)
-        end        
-      end
-      # Now we got the SMSC message id; create pending delivery report.
-      @pdr_storage[pdu.message_id] = mt_message_id            
-    when Pdu::SubmitMultiResponse
-      mt_message_id = @ack_ids[pdu.sequence_number]
-      if !mt_message_id
-        raise "Got SubmitMultiResponse for unknown sequence_number: #{pdu.sequence_number}"
-      end
-      if pdu.command_status != Pdu::Base::ESME_ROK
-        logger.error "Error status in SubmitMultiResponse: #{pdu.command_status}"
-        if @delegate.respond_to?(:message_rejected)
-          @delegate.message_rejected(self, mt_message_id, pdu)
-        end
-      else
-        logger.info "Got OK SubmitMultiResponse (#{pdu.message_id} -> #{mt_message_id})"
-        if @delegate.respond_to?(:message_accepted)
-          @delegate.message_accepted(self, mt_message_id, pdu)
-        end
-      end
-    else
-      super
-    end 
   end
 
   # Send BindTransceiverResponse PDU.
